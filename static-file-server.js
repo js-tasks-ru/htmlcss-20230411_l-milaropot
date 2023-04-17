c5345794ee0b59e2d6d9f3d0e13b523b34d716b4
Mila Ropot <l.milaropot@gmail.com>
2023-04-17 22:17:56 +0300

    delete local

--- static-file-server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express'),
-	app = express();	
-app.set('port', 8080);
-app.use(express.static(__dirname));
-app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
